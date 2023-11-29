0576a155d5863112b593a5980dc10ddc8344d01e
RuyelR <106885758+RuyelR@users.noreply.github.com>
2023-11-29 03:38:34 +0000

    formatted the pages and added sklearn

--- pages/1_Quantitative_Evaluations.py
+++ pages/1_Quantitative_Evaluations.py
@@ -18,31 +18,30 @@ import numpy as np
 
 import streamlit as st
 from streamlit.hello.utils import show_code
+'''
+Accuracy, Precision, Recall, F1 Score: how well the model classifies sentiments
+Confusion Matrix: misclassifying sentiments and which sentiments are often confused
+Cross-Validation: robust and not overfitting.estimate the model's generalization performance.
+Statistical Significance Testing:differences in sentiment are statistically significant
+'''
 
 
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+def confusion_matrix_eval():
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+    pass
 
-    progress_bar.empty()
+def statistical_significance_eval():
+    pass
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+def cross_validation_eval():
+    
+    pass
+
+def multi_metric_eval():
+    pass
 
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.set_page_config(page_title="Quantitative Evaluations", page_icon="📈")
 st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
 st.write(
@@ -51,6 +50,5 @@ Streamlit. We're generating a bunch of random numbers in a loop for around
 5 seconds. Enjoy!"""
 )
 
-plotting_demo()
 
-show_code(plotting_demo)
+# show_code()
---
+++ pages/2_Qualitative_Evaluations.py
@@ -0,0 +1,48 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from urllib.error import URLError
+
+import altair as alt
+import pandas as pd
+
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+"""
+Sentiment Distribution Plots: sentiment balance in datasheet
+Word_clouds: (+,-,~)catagory. prominent factors driving sentiment
+Topic Modeling: LDA to id key topics in each sentiment category
+"""
+
+def sentiment_dist_plots():
+    pass
+
+def world_cloud_eval():
+    pass
+
+def topic_modeling_eval():
+    pass
+
+st.set_page_config(page_title="Qualitative Evaluations", page_icon="📊")
+st.markdown("# DataFrame Demo")
+st.sidebar.header("DataFrame Demo")
+st.write(
+    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
+)
+
+data_frame_demo()
+
+show_code(data_frame_demo)
